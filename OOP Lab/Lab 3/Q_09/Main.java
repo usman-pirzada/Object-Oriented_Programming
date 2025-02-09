8594f0c123e8d21e9d54ba64e06e44fc33f80de0
usman-pirzada <k243000@nu.edu.pk>
2025-02-09 12:43:08 +0500

    Add Retail Store functionality with price storage, discount application, and display features

--- OOP Lab/Lab 3/Q_09/Main.java
+++ OOP Lab/Lab 3/Q_09/Main.java
@@ -1,7 +1,60 @@
 package Q_09;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+
+        Scanner sc = new Scanner(System.in);
+
+        RetailStore price = new RetailStore();
+
+        while (true) {
+            System.out.println("\nWelcome to Discount Bazar!");
+            System.out.println();
+            System.out.println("1) Store price");
+            System.out.println("2) Apply Discount");
+            System.out.println("3) Display Original & Discounted Prices");
+            System.out.println("4) Exit");
+            System.out.print("Enter a number to select an option: ");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println();
+                    price.storePrice(sc);
+                    System.out.println("Price values stored successfully!");
+                    break;
+
+                case 2:
+                    if(price.originalPrice == null) {
+                        System.out.println("No prices stored yet!!");
+                        break;
+                    }
+
+                    System.out.println();
+                    System.out.println("By how much percent do you want to apply discount to all the prices?");
+                    float value = sc.nextFloat();
+                    price.applyDiscount(value);
+                    System.out.println("Discount Applied Successfully!");
+                    break;
+
+                case 3:
+                    if(price.originalPrice == null) {
+                        System.out.println("No prices stored yet!!");
+                        break;
+                    }
+
+                    price.displayPrice();
+                    break;
+
+                case 4:
+                    System.out.println("Good Bye!");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid Input!! Try Again.");
+            }
+        }
     }
 }
--- OOP Lab/Lab 3/Q_09/RetailStore.java
+++ OOP Lab/Lab 3/Q_09/RetailStore.java
@@ -1,5 +1,49 @@
 package Q_09;
 
-public class RetailStore {
+import java.util.Scanner;
 
+public class RetailStore {
+    float[] originalPrice;
+    int arrLength;
+
+    float[] modifiedPrice;
+
+    void storePrice(Scanner sc) {
+        System.out.print("Enter the length of array for storing prices: ");
+        arrLength = sc.nextInt();
+
+        originalPrice = new float[arrLength];
+
+        System.out.println("Enter all the price values separated by spaces:");
+        for (int i = 0; i < originalPrice.length; i++) {
+            originalPrice[i] = sc.nextFloat();
+        }
+    }
+
+    void applyDiscount(float discountPercent) {
+        modifiedPrice = new float[originalPrice.length];
+
+        for(int i = 0; i < originalPrice.length; i++) {
+            modifiedPrice[i] = originalPrice[i] - (originalPrice[i] * discountPercent/100);
+        }
+    }
+
+    void displayPrice() {
+        System.out.println();
+        System.out.print("Original Prices: ");
+        for(float price: originalPrice) {
+            System.out.print(price + "  ");
+        }
+
+        System.out.println();
+        if(modifiedPrice == null) {
+            System.out.println("\nNo modified price!");
+            return;
+        }
+
+        System.out.print("Discounted Prices: ");
+        for(float price: modifiedPrice) {
+            System.out.print(price + "  ");
+        }
+    }
 }
