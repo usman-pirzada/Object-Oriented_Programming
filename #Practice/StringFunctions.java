34186c0927d194147a7d39ce33f54a2c09b476b6
Muhammad Usman Pirzada <k243000@nu.edu.pk>
2025-03-08 11:41:03 +0500

    .

---
+++ #Practice/StringFunctions.java
@@ -0,0 +1,9 @@
+public class StringFunctions {
+    public static void main(String[] args) {
+        String address = "Jaddah, KSA";
+
+        if(address.endsWith("KSA")) {
+            System.out.println("You are living in Kingdom of Saudi Arabia");
+        }
+    }
+}
---
+++ OOP Lab/Lab 5/Q_01/Main.java
@@ -0,0 +1,7 @@
+package Q_01;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
