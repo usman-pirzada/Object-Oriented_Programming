89d51af16179a93c12c778e741d74b04bccd2a17
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-03-18 08:59:58 +0500

    Add files via upload

---
+++ OOP Lab/Lab 7/ExplicitCasting.java
@@ -0,0 +1,10 @@
+public class ExplicitCasting {
+    public static void main(String[] args) {
+        double a = 54.36;
+
+        int b = (int) a;
+
+        System.out.println("Double variable: " + a);
+        System.out.println("After explicitly costing to int: " + b);
+    }
+}
