90dda4b0d522ab48a5837d651fa8b6c6ec2a4b3f
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-03-18 09:48:14 +0500

    Add files via upload

---
+++ OOP Lab/Lab 7/Task05_ObjectCastingScenerio.java
@@ -0,0 +1,19 @@
+class Product {
+    void showDetails() {
+
+    }
+}
+
+class Electronics {
+
+}
+
+class Clothing {
+
+}
+
+public class Task05_ObjectCastingScenerio {
+    public static void main(String[] args) {
+
+    }
+}
