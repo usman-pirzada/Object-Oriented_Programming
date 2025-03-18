55cae9521fe3e51810b83e36b63a3794753eecb1
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-03-18 08:58:46 +0500

    Add files via upload

---
+++ OOP Lab/ImplicitCasting.java
@@ -0,0 +1,13 @@
+public class ImplicitCasting {
+    public static void main(String[] args) {
+        // Implicit Casting
+        int integer;
+
+        integer = 20;
+
+        double double_var = integer;
+
+        System.out.println("Integer Variable: " + integer);
+        System.out.println("After implicit casting to double: " + double_var);
+    }
+}
