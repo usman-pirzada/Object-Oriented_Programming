c2db484fa99e2b22bf3650ffaf27766c8da2ef1d
Muhammad Usman Pirzada <k243000@nu.edu.pk>
2025-03-22 00:45:12 +0500

    .

--- OOP Lab/Lab 7/Task_06.java
+++ OOP Lab/Lab 7/Task_06.java
@@ -1,26 +1,58 @@
 class Vehicle {
+    String brand;
+    int model;
+
+    Vehicle(String brand, int model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
     void showDetails() {
+        System.out.println("Vehicle Brand: " + brand);
+        System.out.println("Model: " + model);
     }
 }
 
 class Car extends Vehicle {
     int noOfSeats;
 
+    Car(String brand,int model, int noOfSeats) {
+        super(brand, model);
+        this.noOfSeats = noOfSeats;
+    }
+
     @Override
     void showDetails() {
+        super.showDetails();
+        System.out.println("No. of Seats: " + noOfSeats);
+        System.out.println();
     }
 }
 
 class Bike extends Vehicle {
     int engineCapacity;
 
+    Bike(String brand, int model, int engineCapacity) {
+        super(brand, model);
+        this.engineCapacity = engineCapacity;
+    }
+
     @Override
     void showDetails() {
+        super.showDetails();
+        System.out.println("Engine Capacity: " + engineCapacity + " CC");
+        System.out.println();
     }
 }
 
 public class Task_06 {
     public static void main(String[] args) {
         
+        Car c = new Car("Toyota", 2020, 5);
+        Bike b = new Bike("Suzuki", 2015, 70);
+
+        c.showDetails();
+        b.showDetails();
+
     }
 }
