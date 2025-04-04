1050792b450eac6f63f1bc0dc049fff729b01327
Muhammad Usman Pirzada <k243000@nu.edu.pk>
2025-04-04 06:56:47 +0500

    .

--- OOP Th/Assignment 2/Q_03/RamzanBox_AI_System.java
+++ OOP Th/Assignment 2/Q_03/RamzanBox_AI_System.java
@@ -12,14 +12,14 @@ public class RamzanBox_AI_System {
 
         // Array of vehicles is passed to the command method, where AI decision-making system will decide the vehicle to choose for delivery.
         System.out.println("******Urgent Delivery******");
-        System.out.println("\tVehicle-01");
+        System.out.println("\tDelivery-01");
         ControlPanel.command(vehicles, "Deliver", 123, true);
 
         System.out.println("\n******Non-urgent Deliveries******");
-        System.out.println("\tVehicle-02");
+        System.out.println("\tDelivery-02");
         ControlPanel.command(vehicles, "Deliver", 654, false);
         
-        System.out.println("\n\tVehicle-03");
+        System.out.println("\n\tDelivery-03");
         ControlPanel.command(vehicles, "Deliver", 987);
 
     }
