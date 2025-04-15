40bb9719a02fdd3f5f90ef9f3869977efdb732f2
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-04-15 10:01:37 +0500

    Add files via upload

---
+++ OOP Lab/Lab 9/Task_05.java
@@ -0,0 +1,21 @@
+interface MessageService {
+
+    void send(String message, String recipient);
+    String receive();
+}
+
+class WhatsApp implements MessageService {
+    void send() {
+
+    }
+
+    String receive() {
+
+    }
+}
+
+public class Task_05 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ OOP Lab/Lab 9/Task_06.java
@@ -0,0 +1,27 @@
+interface BankAccount {
+    void deposit(double amount);
+
+    void withdraw(double amount);
+
+    double getBalance();
+}
+
+class SavingsAccount extends BankAccount {
+    void deposit(double amount) {
+
+    }
+
+    void withdraw(double amount) {
+
+    }
+
+    double getBalance() {
+
+    }
+}
+
+public class Task_06 {
+    public static void main(String[] args) {
+
+    }
+}
