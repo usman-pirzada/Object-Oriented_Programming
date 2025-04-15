5f2f06d3d365323440cbbe2e0387f12785cdd0ca
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-04-15 11:02:47 +0500

    .

--- OOP Lab/Lab 9/Task_06.java
+++ OOP Lab/Lab 9/Task_06.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 interface BankAccount {
     void deposit(double amount);
 
@@ -6,22 +8,62 @@ interface BankAccount {
     double getBalance();
 }
 
-class SavingsAccount extends BankAccount {
-    void deposit(double amount) {
+class SavingsAccount implements BankAccount {
+    private double balance = 0;
 
+    public void deposit(double amount) {
+        this.balance += amount;
+        System.out.println("Amount deposited successfully!");
     }
 
-    void withdraw(double amount) {
-
+    public void withdraw(double amount) {
+        if(amount <= this.balance) {
+            this.balance -= amount;
+            System.out.println("Amount withdrawn successfully!");
+        } else {
+            System.out.println("Can't withdraw. Balance Insufficient!");
+        }
     }
 
-    double getBalance() {
-
+    public double getBalance() {
+        return balance;
     }
 }
 
 public class Task_06 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        SavingsAccount savingsAccount = new SavingsAccount();
+
+        while (true) {
+            // Display menu to the user
+            System.out.println("\nBank Account Menu:");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Get Balance");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+
+            switch(scanner.nextInt()) {
+                case 1: // Deposit
+                    System.out.print("Enter amount to deposit: ");
+                    savingsAccount.deposit(scanner.nextDouble());
+                    break;
+                case 2: // Withdraw
+                    System.out.print("Enter amount to withdraw: ");
+                    savingsAccount.withdraw(scanner.nextDouble());
+                    break;
+                case 3: // Display balance
+                    System.out.println("\nCurrent balance: " + savingsAccount.getBalance());
+                    break;
+                case 4: // Exit
+                    System.out.println("Exiting program. Goodbye!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
