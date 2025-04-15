ce1204c22c27b0432f61f18a7f75585a9c99d67a
Muhammad Usman Pirzada <usman.24231@gmail.com>
2025-04-15 10:41:06 +0500

    .

--- OOP Lab/Lab 9/Task_05.java
+++ OOP Lab/Lab 9/Task_05.java
@@ -5,17 +5,24 @@ interface MessageService {
 }
 
 class WhatsApp implements MessageService {
-    void send() {
+    private String message;
 
+    public void send(String message, String recipient) {
+        this.message = message;
+        System.out.println("Your message has been sent to " + recipient + " successfully");
     }
 
-    String receive() {
-
+    public String receive() {
+        return "\nReceived Message:\n\t" + this.message;
     }
 }
 
 public class Task_05 {
     public static void main(String[] args) {
 
+        WhatsApp wa = new WhatsApp();
+
+        wa.send("Where are you?", "Zeeshan");
+        System.out.println(wa.receive());
     }
 }
