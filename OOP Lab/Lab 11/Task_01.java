c4e43f8cd9df666b3627d769e9a1be71cf8cd107
Muhammad Usman <k243000@nu.edu.pk>
2025-05-05 15:01:49 +0000

    .

--- OOP Lab/Lab 11/Task_01.java
+++ OOP Lab/Lab 11/Task_01.java
@@ -1,5 +1,31 @@
-public class Task_01 {
+import java.util.ArrayList;
+import java.util.List;
+
+class Issue<T> {
+    T issueTitle;
+    T issueDetails;
+
+    Issue(T issueTitle, T issueDetails) {
+        this.issueTitle = issueTitle;
+        this.issueDetails = issueDetails;
+    }
+}
+
+class Task_01 {
+
+    <T> Issue<T> createIssues(T title, T detail) {
+        Issue<T> issue = new Issue<T>(title, detail);
+        return issue;
+    }
+
     public static void main(String[] args) {
 
+        Task_01 task = new Task_01();
+        
+
+        List<Issue <?>> myList = new ArrayList<>();
+        myList.add(task.createIssues(54546, "This ticket is for technical issues."));
+
+        System.out.println(myList.get(0));
     }
 }
