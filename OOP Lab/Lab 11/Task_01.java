db93c0bf13a4edf6f03e5d40ea19df957fcaf5dc
Muhammad Usman Pirzada <k243000@nu.edu.pk>
2025-05-06 00:58:59 +0500

    .

--- OOP Lab/Lab 11/Task_01.java
+++ OOP Lab/Lab 11/Task_01.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Issue<T> {
+    T issueType;
     T issueTitle;
     T issueDetails;
 
-    Issue(T issueTitle, T issueDetails) {
+    Issue(T issueType, T issueTitle, T issueDetails) {
+        this.issueType = issueType;
         this.issueTitle = issueTitle;
         this.issueDetails = issueDetails;
     }
@@ -13,19 +15,27 @@ class Issue<T> {
 
 class Task_01 {
 
-    <T> Issue<T> createIssues(T title, T detail) {
-        Issue<T> issue = new Issue<T>(title, detail);
+    static <T> Issue<T> createIssues(T issueType, T issueTitle, T issueDetails) {
+        Issue<T> issue = new Issue<T>(issueType, issueTitle, issueDetails);
         return issue;
+    }
+
+    static void printIssues(List<Issue<?>> issuesList) {
+        for (Issue<?> issue : issuesList) {
+            System.out.println("\n" + issue.issueType + "\n" + issue.issueTitle + ": " + issue.issueDetails);
+        }
     }
 
     public static void main(String[] args) {
 
         Task_01 task = new Task_01();
-        
 
-        List<Issue <?>> myList = new ArrayList<>();
-        myList.add(task.createIssues(54546, "This ticket is for technical issues."));
+        List<Issue<?>> myList = new ArrayList<>();
 
-        System.out.println(myList.get(0));
+        myList.add(createIssues("HR Issue", "Ticket No. for Salary Query", 65491));
+        myList.add(createIssues("Technical Issue", "Server Down", "Due to frequent breakdown electricity server efficiency has been downgraded!"));
+        myList.add(createIssues("Admin Issue", "Office Supplies Shortage", "Referred to Case# 65184129"));
+
+        printIssues(myList);
     }
 }
--- OOP Lab/Lab 11/Task_02.java
+++ OOP Lab/Lab 11/Task_02.java
@@ -1,5 +1,42 @@
-public class Task_02 {
+import java.util.ArrayList;
+import java.util.List;
+
+class AdmissionRecords<T> {
+    private List<T> records;
+
+    AdmissionRecords() {
+        this.records = new ArrayList<>();
+    }
+
+    void addRecord(T record) {
+        this.records.add(record);
+    }
+
+    void printRecords() {
+        System.out.println();
+        for (T record : records) {
+            System.out.println(record);
+        }
+    }
+}
+
+class Task_02 {
     public static void main(String[] args) {
 
+        AdmissionRecords<String> nameRecords = new AdmissionRecords<>();
+        nameRecords.addRecord("Zeeshan");
+        nameRecords.addRecord("Hamza");
+        nameRecords.addRecord("Bilal");
+        nameRecords.addRecord("Huzaifa");
+
+        AdmissionRecords<Double> scoreRecords = new AdmissionRecords<>();
+        scoreRecords.addRecord(89.2);
+        scoreRecords.addRecord(74.5);
+        scoreRecords.addRecord(87.3);
+        scoreRecords.addRecord(88.0);
+
+
+        nameRecords.printRecords();
+        scoreRecords.printRecords();
     }
 }
